394a0db171e308216552dfcedb0693407db7b5aa
lzeeorno <43123195+lzeeorno@users.noreply.github.com>
2022-02-27 16:37:57 +0800

    fuchen-leon

--- RNN/RNN.py
+++ RNN/RNN.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import numpy as np
-train = pd.read_csv("train.csv")
-test = pd.read_csv("test.csv")
-train.head()
+train = pd.read_csv("./data/train.csv")
+test = pd.read_csv("./data/test.csv")
+print(train.head())
